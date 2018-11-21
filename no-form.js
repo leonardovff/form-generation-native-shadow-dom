32edbec8167c3d8038e212b82929e8d2ea147222
Leonardo Victor <leonardovff@gmail.com>
2018-11-21 02:28:46 -0300

    first version with initical competizion and generation

---
+++ no-form.js
@@ -0,0 +1,18 @@
+function createElement(field){
+  // style and control to create diferrent inputs
+  let el = document.createElement(`no-form-field`);
+  el.setAttribute('field', JSON.stringify(field));
+  return el;
+}
+function run(selector, data){
+  const appDiv = document.querySelector(selector);
+
+  for(let i = 0; i < data.fields.length; i++){
+    let field = data.fields[i];
+    field.dom = createElement(field);
+    appDiv.appendChild(field.dom);
+  }
+  return { 
+    getValue: () => data.fields.map(field => field.value)
+  }
+}
